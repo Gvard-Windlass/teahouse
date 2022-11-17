4d540e469339e5c1503ca0a67ea02fcb26e1fab8
Gvard Windlass <gvard.windlass@gmail.com>
2022-11-17 15:21:54 +0300

    Add static files settings

--- teahouse/settings.py
+++ teahouse/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from configurations import Configuration, values
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,6 +68,12 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATIC_ROOT = values.Value()
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
 ]
 
 WSGI_APPLICATION = 'teahouse.wsgi.application'
