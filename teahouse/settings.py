8b39b54749a8f4be8cc10ecf8d1aa4c456a96398
Gvard Windlass <gvard.windlass@gmail.com>
2022-11-21 13:10:32 +0300

    Add media settings

--- teahouse/settings.py
+++ teahouse/settings.py
@@ -76,6 +76,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
 WSGI_APPLICATION = 'teahouse.wsgi.application'
 
 
