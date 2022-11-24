95b89f36f778d079a390713587adc2ce1d14ea24
Gvard Windlass <gvard.windlass@gmail.com>
2022-11-24 14:42:38 +0300

    Add tea products display test

--- store/test/test_pages.py
+++ store/test/test_pages.py
@@ -3,12 +3,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
+from store.models import Tea
+
+firefox_dev_binary = FirefoxBinary('C:\Program Files\Firefox Developer Edition\\firefox.exe')
+driver_path = 'C:\Dev\django_dev_1\geckodriver.exe'
+
 class TestHomePage(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        firefox_dev_binary = FirefoxBinary('C:\Program Files\Firefox Developer Edition\\firefox.exe')
-        driver_path = 'C:\Dev\django_dev_1\geckodriver.exe'
         cls.selenium = WebDriver(firefox_binary=firefox_dev_binary, executable_path=driver_path)
         cls.selenium.implicitly_wait(10)
 
@@ -40,3 +43,43 @@ class TestHomePage(StaticLiveServerTestCase):
         self.assertFalse(sidebar_toggle.is_displayed())
         self.assertTrue(sidebar.is_displayed())
 
+
+class TestProductDisplay(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(firefox_binary=firefox_dev_binary, executable_path=driver_path)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    
+    def test_tea_display(self):
+        Tea.objects.create(
+            name='test tea 1',
+            price = 300.5,
+            image = 'product_images/black1.jpg',
+            description = 'tea for testing',
+            product_type = 'Tea',
+            tea_type = 'Black',
+            tea_year = 2022,
+            tea_amount = 300.5
+        )
+
+        Tea.objects.create(
+            name='test tea 2',
+            price = 200.5,
+            image = 'product_images/black2.jpg',
+            description = 'tea for testing',
+            product_type = 'Tea',
+            tea_type = 'Black',
+            tea_year = 2021,
+            tea_amount = 50.5
+        )
+
+        self.selenium.get(f'{self.live_server_url}/tea/')
+        tea_products = self.selenium.find_elements(By.CLASS_NAME, 'card')
+        self.assertEqual(len(tea_products), 2)
