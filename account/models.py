28412b0cd5d47a6c007a2bc8525cb2b1f3d8e669
Gvard Windlass <gvard.windlass@gmail.com>
2022-11-26 13:54:25 +0300

    Add Customer model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birthday = models.DateField(null=True)
+    avatar = models.ImageField(upload_to='user_images/', null=True)
+
+
+@receiver(post_save, sender=User)
+def create_customer(sender, instance, created, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_customer(sender, instance, **kwargs):
+    instance.customer.save()
---
+++ account/test/test_models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from account.models import *
+
+class TestCustomerModel(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            username = 'test user',
+            password = 'test password',
+        )
+        user.customer.birthday = '2000-01-01'
+        user.save()
+
+
+    def test_create_customer(self):
+        user = User.objects.get(username='test user')
+        self.assertIsInstance(user.customer, Customer)
+        self.assertEqual(str(user.customer.birthday), '2000-01-01')
