93854763e20771185e29daac1cbf53177cad6dcb
Gvard Windlass <gvard.windlass@gmail.com>
2022-11-28 15:01:00 +0300

    Add auth templates

--- teahouse/settings.py
+++ teahouse/settings.py
@@ -39,9 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'catalogue',
     'account'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
