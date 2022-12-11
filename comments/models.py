f610504bf1b0eb3c558f75538592bee3b02533d0
Gvard Windlass <gvard.windlass@gmail.com>
2022-12-11 12:04:53 +0300

    Add Comment model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalogue.models import Product
 
-# Create your models here.
+from mptt.models import MPTTModel, TreeForeignKey
+
+
+class Comment(MPTTModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    parent = TreeForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
