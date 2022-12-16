2cbeb68f7505512b06f8f5cc0d58463820c5e906
Gvard Windlass <gvard.windlass@gmail.com>
2022-12-16 10:56:40 +0300

    Add Article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    thumbnail = models.ImageField(upload_to='article_images/', null=True, blank=True)
+    summary = models.TextField()
+    body = models.TextField()
+    publication_date = models.DateField(auto_now_add=True)
