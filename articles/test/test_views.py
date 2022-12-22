4bc08a776470a9d1982b9b0c2e30dda8397c4263
Gvard Windlass <gvard.windlass@gmail.com>
2022-12-22 11:10:08 +0300

    Add tests for article detail view

--- articles/test/test_views.py
+++ articles/test/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 
-from articles.models import Article
+from test.factories import ArticleFactory
 
 class TestArticleListView(TestCase):
     def setUp(self):
@@ -9,3 +9,18 @@ class TestArticleListView(TestCase):
     def test_article_list_view(self):
         response = self.client.get('/articles/')
         self.assertEqual(response.status_code, 200)
+
+
+class TestArticleDetailView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_article_exist_detail(self):
+        ArticleFactory.create()
+        response = self.client.get('/articles/1/')
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_article_not_exist_detail(self):
+        response = self.client.get('/articles/2/')
+        self.assertEqual(response.status_code, 404)
