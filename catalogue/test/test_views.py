aae9fe2029a6660635fb8cc2846cad01472e5dda
Gvard Windlass <gvard.windlass@gmail.com>
2022-12-22 11:19:01 +0300

    Add additional tests for catalogue views

--- catalogue/test/test_views.py
+++ catalogue/test/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 from catalogue.models import Tea
-from test.factories import TeaFactory
+from test.factories import TeaFactory, UtensilFactory
 
 class TestTeaListView(TestCase):
     def setUp(self):
@@ -8,12 +8,27 @@ class TestTeaListView(TestCase):
 
     
     def test_tea_list_view_specific(self):
-        response = self.client.get('/product_catalogue/Black/')
+        response = self.client.get('/product_catalogue/Tea/Black/')
         self.assertEqual(response.status_code, 200)
 
 
     def test_tea_list_view_all(self):
-        response = self.client.get('/product_catalogue/')
+        response = self.client.get('/product_catalogue/Tea/')
+        self.assertEqual(response.status_code, 200)
+
+
+class TestUtensilListView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    
+    def test_utensil_list_view_specific(self):
+        response = self.client.get('/product_catalogue/Utensil/Cup/')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_utensil_list_view_all(self):
+        response = self.client.get('/product_catalogue/Utensil/')
         self.assertEqual(response.status_code, 200)
 
 
@@ -22,11 +37,31 @@ class TestTeaDetailView(TestCase):
         self.client = Client()
 
     
-    def test_tea_detail_view(self):
+    def test_tea_exist_detail_view(self):
         TeaFactory.create()
-
         response = self.client.get('/tea/1/')
         self.assertEqual(response.status_code, 200)
+
+    
+    def test_tea_not_exist_detail_view(self):
+        response = self.client.get('/tea/2/')
+        self.assertEqual(response.status_code, 404)
+
+
+class TestUtensilDetailView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    
+    def test_utensil_exist_detail_view(self):
+        UtensilFactory.create()
+        response = self.client.get('/utensil/1/')
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_utensil_not_exist_detail_view(self):
+        response = self.client.get('/utensil/2/')
+        self.assertEqual(response.status_code, 404)
 
 
 class TestProductSearchView(TestCase):
@@ -34,6 +69,6 @@ class TestProductSearchView(TestCase):
         self.client = Client()
 
     
-    def test_tea_detail_view(self):
+    def test_product_search_view(self):
         response = self.client.get('/product_search/?q=tea')
         self.assertEqual(response.status_code, 200)
