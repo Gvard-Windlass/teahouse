6d2cfd607c7909571fd273a25b5381ca5ee19d16
Gvard Windlass <gvard.windlass@gmail.com>
2022-12-23 13:30:11 +0300

    Add search integration tests

--- catalogue/test/test_pages.py
+++ catalogue/test/test_pages.py
@@ -100,6 +100,65 @@ class TestProductsDisplay(StaticLiveServerTestCase):
         WebDriverWait(self.selenium, 10).until(EC.url_changes(current_url))
 
 
+class TestProductsSearch(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(firefox_binary=FirefoxBinary(setup.FIREFOX_BINARY_PATH), executable_path=setup.DRIVER_PATH)
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        TeaFactory.create_batch(40)
+
+    
+    def test_product_search(self):
+        self.selenium.get(f'{self.live_server_url}/')
+        
+        query_imput = self.selenium.find_element(By.CSS_SELECTOR, 'input[name=q]')
+        query_imput.send_keys('tea')
+        
+        current_url = self.selenium.current_url
+        query_imput.send_keys(Keys.RETURN)
+        WebDriverWait(self.selenium, 10).until(EC.url_changes(current_url))
+
+        tea_products = self.selenium.find_elements(By.CLASS_NAME, 'card')
+        self.assertEqual(len(tea_products), 10)
+
+    
+    def test_search_pagination(self):
+        url = reverse('product_search')
+        q = 'tea'
+        self.selenium.get(f'{self.live_server_url}{url}?q={q}')
+
+        pagination = self.selenium.find_element(By.CLASS_NAME, 'pagination')
+        self.assertTrue(pagination.is_displayed())
+
+        pg_next = self.selenium.find_element(By.ID, 'page-next')
+        current_url = self.selenium.current_url
+        pg_next.click()
+        WebDriverWait(self.selenium, 10).until(EC.url_changes(current_url))
+        
+        pg_end = self.selenium.find_element(By.ID, 'page-end')
+        current_url = self.selenium.current_url
+        pg_end.click()
+        WebDriverWait(self.selenium, 10).until(EC.url_changes(current_url))
+
+        pg_prev = self.selenium.find_element(By.ID, 'page-prev')
+        current_url = self.selenium.current_url
+        pg_prev.click()
+        WebDriverWait(self.selenium, 10).until(EC.url_changes(current_url))
+
+        pg_begin = self.selenium.find_element(By.ID, 'page-begin')
+        current_url = self.selenium.current_url
+        pg_begin.click()
+        WebDriverWait(self.selenium, 10).until(EC.url_changes(current_url))
+
+
 class TestProductDetailPage(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
